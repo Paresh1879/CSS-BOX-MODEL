d706eff2a0465f6456c73f43c3959c87c6182f93
Paresh1879 <natarajan.paresh@gmail.com>
2022-04-23 11:48:29 +0530

    Survey Form

---
+++ DOM/script.js
@@ -0,0 +1,4 @@
+var num1=document.getElementById("first").value;
+var num2=document.getElementById("second").value;
+var result=parseInt(num1)+parseInt(num2);
+document.getElementsByClassName("output")[0].innerHTML="Adding is"+result;
